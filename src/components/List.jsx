528706464a83c7eeee440f67d8131f97dcea36f1
Davinder Kumar <abhiverma97819@gmail.com>
2023-07-01 14:32:23 +0530

    some changes made

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -59,7 +59,7 @@ const List = ({ places, isLoading }) => {
                 direction={'column'}
             >
                 {
-                    places && places.map((place, i) => <PlaceDetail place={place} key={i} />)
+                    places && places.map((place) => <PlaceDetail place={place} key={i} />)
                 }
             </Flex>
         </Flex>
